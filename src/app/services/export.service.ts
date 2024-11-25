1190b149fbc9b52b037f149885f77935194646b6
EduNewgate <eduardo.moreno.93@gmail.com>
2024-11-25 16:38:12 +0100

    Excel export

--- src/app/services/export.service.ts
+++ src/app/services/export.service.ts
@@ -13,18 +13,15 @@ export class ExportService {
   constructor() { }
 
   public exportToExcel(cardData: CardDetail[]) {
-    const ws: XLSX.WorkSheet = XLSX.utils.json_to_sheet(
-      [{cardName: 'Card name', set: 'Set name'}]
-    );
+    let list: {cardName: string, set: string}[] = [];
+    list.push({ cardName: 'Card name', set: 'Set Name' });
+    const ws: XLSX.WorkSheet = XLSX.utils.json_to_sheet(list);
 
     for (let index = 0; index < cardData.length; index++) {
       const element: CardDetail = cardData[index];
-      XLSX.utils.sheet_add_json(ws,
-        [
-          {cardName: element.card.name, set: element.card.set.name }
-        ]
-      );
+      list.push({cardName: element.card.name, set: element.card.set.name});
     }
+    XLSX.utils.sheet_add_json(ws, list);
     const workbook: XLSX.WorkBook = { Sheets: { Sheet1: ws }, SheetNames: ['Sheet1'] };
     XLSX.writeFile(workbook, `prueba${EXCEL_EXTENSION}`);
   }
